71b1038b9da1e0b2604d5927de15e3b81e133e08
EddieHooks36 <eddiehooks36@gmail.com>
2022-12-11 20:27:36 -0800

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Script file");
+
+let platformName = "Netflix";
+let typeOfMedia = "Netflix Series";
+let seriesName = "The Umbrella Academy";
+let numberOfSeasons = "3 seasons";
+let episode = "8 episodes";
+let mainCharacters = "7 main characters";
+let genre = "Sci-Fi";
+let geolocation = "Time travel";
+let rating = "TV-MA";
+
+document.write(`<p>You can scroll through ${platformName} and in the ${typeOfMedia}, I like to watch ${seriesName}. They currently have ${numberOfSeasons} and ${episode} per seaseon as well as ${mainCharacters}. The type of genre is ${genre}, perfect for comic book lovers. It is also based around ${geolocation}. The rating on the series is ${rating}, which I would think is okay for 13 years old and older.
+</p>`)
+
